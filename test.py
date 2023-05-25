6db9584286644f0918556319a6e755933a743888
Nguyễn Khánh Thuận <leeboykt@gmail.com>
2023-05-25 19:54:25 +0700

    add requirements

--- test.py
+++
@@ -1,8 +0,0 @@
-from detection.object_detection import game_detection
-from PIL import Image
-
-file = open("2023-04-23 (1).png", "rb")
-image = Image.open(file)
-
-results = game_detection(image)
-print(results)
