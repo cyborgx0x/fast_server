dfe10b32fa633c2f9972da142a760e0bd56248be
Nguyễn Khánh Thuận <leeboykt@gmail.com>
2023-07-05 17:00:03 +0700

    Update ocr.py

--- detection/ocr.py
+++ detection/ocr.py
@@ -24,4 +24,4 @@ def bypass_captcha(image):
     pixel_values = pixel_values.to("cuda:0")
     generated_ids = model.generate(pixel_values)
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-    return generated_text
+    return generated_text.lower()
