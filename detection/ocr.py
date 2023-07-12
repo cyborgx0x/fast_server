14ca380fa7cf0d68d58840fe16153aa27e00ded4
Nguyễn Khánh Thuận <leeboykt@gmail.com>
2023-07-13 00:18:36 +0700

    Update ocr.py

--- detection/ocr.py
+++ detection/ocr.py
@@ -2,7 +2,7 @@ from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from detection.distort import negative_barrel
 processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-printed')
 model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-printed')
-# model.to("cuda:0")
+model.to("cuda:0")
 import numpy as np
 from PIL import Image
 import cv2
